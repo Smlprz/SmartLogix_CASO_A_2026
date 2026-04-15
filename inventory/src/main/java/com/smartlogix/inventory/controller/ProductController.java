ac5f585fbdf3b42e1471d62da0ffb083ced1c2b3
vicenteoyarzun <vi.oyarzuns@duocuc.cl>
2026-04-15 12:51:15 -0400

    cambio menor 2 producto

--- inventory/src/main/java/com/smartlogix/inventory/controller/ProductController.java
+++ inventory/src/main/java/com/smartlogix/inventory/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.smartlogix.inventory.model.Product;
 import com.smartlogix.inventory.service.ProductService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,7 +25,7 @@ public class ProductController {
 
     @PostMapping
     @Operation(summary = "Añadir producto nuevo")
-    public Product create(@RequestBody Product product) { return service.save(product); }
+    public Product create(@Valid @RequestBody Product product) { return service.save(product); }
 
     @PutMapping("/{id}")
     @Operation(summary = "Modificar un producto existente")
